6ba399f014fd30c6bc348f274021f37a6fb61028
Naledi Madlopha <naledi.madlopha@gmail.com>
2023-04-20 06:20:15 +0200

    Update student_number field to id. This is to get rid of the duplicate use of the term student in the student object

--- src/main/java/com/sms/app/model/Student.java
+++ src/main/java/com/sms/app/model/Student.java
@@ -19,7 +19,7 @@ public class Student {
                     @Parameter(name = StudentIdGenerator.VALUE_PREFIX_PARAMETER, value = "Stu"),
                     @Parameter(name = StudentIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%07d")
             })
-    private String STUDENT_NUMBER;
+    private String id;
     @Column(name = "first_name", nullable = false)
     private String firstName;
     @Column(name = "last_name", nullable = false)
@@ -30,8 +30,8 @@ public class Student {
     public Student() {
     }
 
-    public Student(String studentNumber, String firstName, String lastName, String email) {
-        this.STUDENT_NUMBER = studentNumber;
+    public Student(String id, String firstName, String lastName, String email) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
